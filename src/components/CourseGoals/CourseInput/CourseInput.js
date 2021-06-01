0207e6d9affe923f1a40501d0508411d11b877e2
lucasgbeleza <lucasguilb@gmail.com>
2021-06-01 07:19:48 -0300

    Working with CSS Classes dynamically

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    event.target.value.trim().length> 0 ? setIsValid(true) : setIsValid(false);
+  const goalInputChangeHandler = (event) => {
+    event.target.value.trim().length > 0 ? setIsValid(true) : setIsValid(false);
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    if(enteredValue.trim().length === 0){
+    if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
@@ -23,14 +23,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}} >Course Goal</label>
-        <input  style=
-          {{
-            borderColor: !isValid? 'red' : 'black', 
-            background: !isValid ? 'almondblanche':'transparent'
-          }} 
-          type="text" onChange={goalInputChangeHandler} />
+      <div className={`formControl ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
